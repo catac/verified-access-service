b3152903242c5d2451b5a7914ac8040b0ec17b85
Catalin CIRSTOIU <ccirstoiu@pictet.com>
2020-11-12 11:46:31 +0100

    files controller - add support for mime types

--- src/main/java/net/catac/verifiedaccess/FilesController.java
+++ src/main/java/net/catac/verifiedaccess/FilesController.java
@@ -4,7 +4,10 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,6 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ResponseStatusException;
 
 import javax.annotation.PostConstruct;
+import javax.servlet.http.HttpServletRequest;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -30,15 +34,29 @@ public class FilesController {
         logger.info("Serving files from " + filesDir + " ...");
     }
 
+    private MediaType findContentType(Path path, HttpServletRequest request) {
+        String fullName = path.toAbsolutePath().toFile().getAbsolutePath();
+        String contentType = request.getServletContext().getMimeType(fullName);
+        if(contentType == null){
+            contentType = "application/octet-stream";
+        }
+        return MediaType.parseMediaType(contentType);
+    }
+
     @RequestMapping("/files/{fileName}")
-    public byte[] file(@PathVariable("fileName") String fileName) throws IOException {
+    public ResponseEntity<byte[]> file(@PathVariable("fileName") String fileName, HttpServletRequest request) throws IOException {
         String sanitizedName = fileName.replaceAll("[^a-zA-Z0-9-_\\.]", "_");
         Path path = Paths.get(filesDir + "/" + sanitizedName);
-        if(Files.notExists(path)){
+        if (Files.notExists(path)) {
             throw new ResponseStatusException(
                     HttpStatus.NOT_FOUND, "File not found"
             );
         }
-        return Files.readAllBytes(path);
+
+        return ResponseEntity
+                .ok()
+                .contentType(findContentType(path, request))
+                .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"" + sanitizedName + "\"")
+                .body(Files.readAllBytes(path));
     }
 }
